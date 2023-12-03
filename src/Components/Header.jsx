7a17056cf96f43fc403e70a96c61a2152f25fba9
sarahganda <69975401+sarahganda@users.noreply.github.com>
2023-12-03 12:51:58 +0700

    koreksi

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ const Header = ()=>  {
     <nav className="navbar navbar-expand-lg custom_nav-container ">
       <a className="navbar-brand" href="index.html">
         <span>
-          Neogym
+          Logo/Nama Toko
         </span>
       </a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,14 +20,11 @@ const Header = ()=>  {
             <li className="nav-item active">
               <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
             </li>
-            <li className="nav-item ">
-              <a className="nav-link" href="why.html"> Why us </a>
+            <li className="nav-item">
+              <a className="nav-link" href="product.html"> product</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="trainer.html"> trainers</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="contact.html"> Contact Us</a>
+              <a className="nav-link" href="about.html"> About Us</a>
             </li>
           </ul>
           <div className="user_option">
