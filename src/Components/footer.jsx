321b691ab8485f4f7fce56c117da9979e1d2acc4
letita24 <letitiaapantouw240503@gmail.com>
2023-12-04 03:22:15 +0800

    html footer ke footerjsx

---
+++ src/Components/footer.jsx
@@ -0,0 +1,16 @@
+const footer = () => {
+  return (
+    <>{/*Footer*/}
+<footer id="landing-footer" className="clearfix">
+  <div className="row clearfix">
+    <p id="copyright" className="col-2">Made with love by <a href="https://www.shapingrain.com">ShapingRain</a></p>
+    {/*Social Icons in Footer*/}
+  </div>
+</footer>
+{/*End of Footer*/}
+
+</>
+  )
+}
+
+export default footer
