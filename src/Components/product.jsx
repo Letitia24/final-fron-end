2c9d65050a5b65f4e40dc1915c555f79a44d5193
letita24 <letitiaapantouw240503@gmail.com>
2023-12-03 22:56:47 +0800

    menambahkan html ke dalam product

---
+++ src/Components/product.jsx
@@ -0,0 +1,78 @@
+const product = () => {
+  return (
+    <>{/*Introduction*/}
+<section id="about" className="introduction scrollto">
+  <div className="row clearfix">
+    <div className="col-3">
+      <div className="section-heading">
+        <h3>SUCCESS</h3>
+        <h2 className="section-title">Product</h2>
+        <p className="section-subtitle">menerima jasa mengedit foto, video, dan pengetikan</p>
+      </div>
+    </div>
+    <div className="col-2-3">
+      {/*Icon Block*/}
+      <div className="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.1s">
+        {/*Icon*/}
+        <div className="icon">
+          <i className="fa-solid fa-print" />
+        </div>
+        {/*Icon Block Description*/}
+        <div className="icon-block-description">
+          <h4>HTML5 &amp; CSS3</h4>
+          <p>Has ne tritani atomorum conclusionemque, in dolorum volumus cotidieque eum. At vis choro
+            neglegentur iudico</p>
+        </div>
+      </div>
+      {/*End of Icon Block*/}
+      {/*Icon Block*/}
+      <div className="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.3s">
+        {/*Icon*/}
+        <div className="icon">
+          <i className="fa fa-bolt fa-2x" />
+        </div>
+        {/*Icon Block Description*/}
+        <div className="icon-block-description">
+          <h4>Easy to Use</h4>
+          <p>Cu vero ipsum vim, doctus facilisi sea in. Eam ex falli honestatis repudiandae, sit
+            detracto mediocrem disputationi</p>
+        </div>
+      </div>
+      {/*End of Icon Block*/}
+      {/*Icon Block*/}
+      <div className="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.5s">
+        {/*Icon*/}
+        <div className="icon">
+          <i className="fa fa-tablet fa-2x" />
+        </div>
+        {/*Icon Block Description*/}
+        <div className="icon-block-description">
+          <h4>Fully Responsive</h4>
+          <p>Id porro tritani recusabo usu, eum intellegam consequuntur et. Fugit debet ea sit, an pro
+            nemore vivendum</p>
+        </div>
+      </div>
+      {/*End of Icon Block*/}
+      {/*Icon Block*/}
+      <div className="col-2 icon-block icon-top wow fadeInUp" data-wow-delay="0.5s">
+        {/*Icon*/}
+        <div className="icon">
+          <i className="fa fa-rocket fa-2x" />
+        </div>
+        {/*Icon Block Description*/}
+        <div className="icon-block-description">
+          <h4>Parallax Effect</h4>
+          <p>Id porro tritani recusabo usu, eum intellegam consequuntur et. Fugit debet ea sit, an pro
+            nemore vivendum</p>
+        </div>
+      </div>
+      {/*End of Icon Block*/}
+    </div>
+  </div>
+</section>
+{/*End of Introduction*/}
+</>
+  )
+}
+
+export default product
