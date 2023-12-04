60e9595acfe52e417ebb61ce9e0b85d347459bff
gijohardent <s2200152@unklab.ac.id>
2023-12-04 08:59:09 +0800

    edit header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,45 +1,35 @@
-
-
-const Header = ()=>  {
+const Header = () => {
   return (
     <header id="banner" className="scrollto clearfix" data-enllax-ratio=".5">
-  <div id="header" className="nav-collapse">
-    <div className="row clearfix">
-      <div className="col-1">
-        <h1>picelCraft</h1>
-        {/*Main Navigation*/}
-        <nav id="nav-main">
-          <ul>
-            <li>
-              <a href="#banner">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#gallery">product</a>
-            </li>
-          </ul>
-        </nav>
-        {/*End of Main Navigation*/}
-        <div id="nav-trigger"><span /></div>
-        <nav id="nav-mobile" />
+      <div id="header" className="nav-collapse">
+        <div className="row clearfix">
+          <div className="col-1">
+            <h1>picelCraft</h1>
+            {/*Main Navigation*/}
+            <nav id="nav-main">
+              <ul>
+                <li>
+                  <a href="#banner">Home</a>
+                </li>
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a href="#gallery">product</a>
+                </li>
+              </ul>
+            </nav>
+            {/*End of Main Navigation*/}
+            <div id="nav-trigger">
+              <span />
+            </div>
+            <nav id="nav-mobile" />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>{/*End of Header*/}
-  {/*Banner Content*/}
-  <div id="banner-content" className="row clearfix">
-    <div className="col-38">
-      <div className="section-heading">
-        <h1>welcome to picelCraft</h1>
-        <h2>Jadikan setiap cetakan spesial, di mana kreativitas 
-          bertemu dengan kualitas untuk memberikan sentuhan unik pada ide-ide Anda!</h2>
-      </div>
-    </div>
-  </div>{/*End of Row*/}
-</header>
-
-  )
-}
+      {/*End of Header*/}
+    </header>
+  );
+};
 
 export default Header;
