966927c85f40ebba43dea2e4b24119660766b4ab
gijohardent <s2200152@unklab.ac.id>
2023-12-04 08:57:46 +0800

    add home

---
+++ src/Components/home.jsx
@@ -0,0 +1,17 @@
+const home = () => {
+  return (
+    <div id="banner-content" className="row clearfix">
+      <div className="col-38">
+        <div className="section-heading">
+          <h1>A FREE AND SIMPLE LANDING PAGE</h1>
+          <h2>Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects, enjoy!</h2>
+        </div>
+      </div>
+      <a href="#" className="button">
+        START CREATING TODAY
+      </a>
+    </div>
+  );
+};
+
+export default home;
