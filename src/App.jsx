512d5b6a4ba43a449ddaee67f89d4db31c2f5806
gijohardent <s2200152@unklab.ac.id>
2023-12-04 09:01:06 +0800

    edit App_jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Header from './Components/Header';
-import Home from './Components/Home';
-import About from './Components/AboutUs';
-import Product from './Components/product';
-import Footer from './Components/footer';
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import About from "./Components/AboutUs";
+import Product from "./Components/product";
+import Footer from "./Components/footer";
 
 function App() {
   return (
